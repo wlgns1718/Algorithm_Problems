92036f24728b1b9b03a6edec3a45f03b3c2ab432
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2023-03-18 16:30:51 +0900

    [Silver III] Title: N과 M (1), Time: 228 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,20 +1,23 @@
 import sys
+
 input = sys.stdin.readline
 
-def sol(select,idx,M,N,nums):
+def sol(idx,select):
     if idx == M:
         for i in range(M):
-                print(nums[i],end=" ")
+            print(nums[i],end=" ")
         return print()
+
     for i in range(1,N+1):
         if select[i] == True:
             continue
-        select[i] = True
         nums[idx] = i
-        sol(select,idx+1,M,N,nums)
+        select[i] = True
+        sol(idx+1,select)
         select[i] = False
 
-N, M = map(int, input().split())
-select = [False]*(N+1)
-nums = [0]*(N+1)
-sol(select,0,M,N,nums)
+N, M = map(int,input().split())
+
+nums = [0]*M
+select = [False] * (N+1)
+sol(0,select)
