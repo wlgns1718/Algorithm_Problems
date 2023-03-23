c2eb683074b9a6418c68b1fcb61604ae965e378a
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2023-03-23 13:16:16 +0900

    [level 2] Title: 숫자의 표현, Time: 4.74 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    for i in range(1,n+1):
+        total = 0
+        for j in range(i,n+1):
+            total += j
+            if total == n:
+                answer += 1
+            elif n < total:
+                break
+    return answer
