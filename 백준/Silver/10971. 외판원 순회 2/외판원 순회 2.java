6360c4a2dc3b3b01dd0ed032ee27223adc83a893
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2023-03-30 14:30:49 +0900

    [Silver II] Title: 외판원 순회 2, Time: 224 ms, Memory: 17068 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,67 +1,62 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    
-    static int N;
-    static int[][] w;
-    static int answer;
-    static int start_city;
-    static int tmp;
-    static boolean[] visited;
-    
-    static void solve(int city,int n) {
-        
-        if(n==N-1) {
-            if(w[city][start_city]!=0) {  // 현재 도시에서 출발 도시로 갈 수 있으면 최솟값 갱신
-                tmp += w[city][start_city];
-                answer = answer > tmp ? tmp : answer;
-                tmp -= w[city][start_city];
-            }
-            return;
-        }
-        
-        for(int i=0;i<N;i++) {
-            if(w[city][i]!=0 && !visited[i]) { // city 에서 i로 갈 수 있고, 방문하지 않았으면
-                tmp+=w[city][i];
-                visited[i]=true;
-                solve(i,n+1);
-                tmp-=w[city][i];
-                visited[i]=false;
-            }
-        }
-        
-        return;
-        
-    }
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        
-        N = Integer.parseInt(br.readLine());
-        w = new int[N][N];
-        answer = Integer.MAX_VALUE;
-        
-        for(int i=0;i<N;i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<N;j++) {
-                w[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        
-        for(int i=0;i<N;i++) {
-            tmp = 0;
-            visited = new boolean[N];
-            start_city = i;
-            visited[i]=true;
-            solve(i,0);
-        }
-        
-        System.out.println(answer);
-        
-        
-    }
-}    
+	static boolean[] select;
+	static int[] nums;
+	static int[][] price;
+	static int answer = Integer.MAX_VALUE;
+	static int N,temp;
+	
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		select = new boolean[N];
+		nums = new int[N];
+		price = new int[N][N];
+		for(int i = 0 ; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j=0; j<N; j++) {
+				price[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		sol(0);
+		System.out.println(answer);
+	}
+	static void sol(int idx) {
+		if(idx == N) {
+			temp = 0;
+			for (int i = 0; i < N; i++) {
+				if(i == N-1) {
+					if(price[nums[i]][nums[0]]==0) {
+						return;
+					}
+					temp += price[nums[i]][nums[0]];
+					continue;
+				}
+				if (price[nums[i]][nums[i+1]] == 0) {
+					return;
+				}
+				temp += price[nums[i]][nums[i+1]];
+				
+			}
+			if(answer > temp) {
+				answer = temp;
+			}
+			return;
+		}
+		
+		for (int i = 0; i<N; i++) {
+			if(select[i]==true) {
+				continue;
+			}
+			nums[idx] = i;
+			select[i] = true;
+			sol(idx + 1);
+			select[i] = false;
+			if(i==0) break;
+		}
+	}
+}
