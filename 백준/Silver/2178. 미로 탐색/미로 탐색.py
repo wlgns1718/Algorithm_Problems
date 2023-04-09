57c92b981df3e33451a1bf959fde274d7f348227
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2023-04-09 11:11:47 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34192 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+answer = []
+def bfs(queue,visit):
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+
+    while queue:
+        cur = queue.popleft()
+        for idx in range(4):
+            nx = cur[0]+dx[idx]
+            ny = cur[1] + dy[idx]
+            if nx == N-1 and ny == M-1:
+                answer.append(cur[2]+1)
+                continue
+            if nx < 0 or ny < 0 or nx >= N or ny >= M: continue
+            if ls[nx][ny] == 0: continue
+            if (nx,ny) in visit: continue
+            queue.append([nx,ny,cur[2]+1])
+            visit.add((nx,ny))
+
+input = sys.stdin.readline
+N, M = map(int,input().split())
+
+ls =[[0] * M for _ in range(N)]
+for i in range(N):
+    temp = input().rstrip()
+    for j in range(M):
+        ls[i][j] = int(temp[j])
+queue = deque()
+visit = set()
+queue.append([0,0,1])
+visit.add((0,0))
+bfs(queue,visit)
+print(min(answer))
