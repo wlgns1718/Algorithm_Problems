cd8348b1f59182a00a3629528de87266bfb2a801
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2023-04-09 10:57:22 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+
+{
+
+    char str;
+
+    
+
+    while(scanf("%c", &str) != -1)
+
+    {
+
+        printf("%c", str);
+
+    }
+
+    
+
+    return 0;
+
+}
