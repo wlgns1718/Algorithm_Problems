5ec06c877029077e8eda70e7e2db8f5425137ab2
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2023-07-20 15:43:24 +0900

    [Gold IV] Title: 장미, Time: 104 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Gold/3343. 장미/장미.py
+++ 백준/Gold/3343. 장미/장미.py
@@ -1,24 +1,24 @@
 import sys
-from math import ceil
- 
+import math
 input = sys.stdin.readline
- 
-N,AC,AP,BC,BP = map(int,input().split())
 
-if AP*BC < BP*AC:
-    AC,AP,BC,BP = BC,BP,AC,AP
- 
- 
-answer = float('inf')
- 
-for A_COUNT in range(BC):
-    B_COUNT = ceil((N-A_COUNT*AC)/BC)
-    isover = False
-    if B_COUNT<0:
-        B_COUNT = 0
-        isover = True
-    answer = min(answer, A_COUNT*AP + B_COUNT*BP)
-    if isover:
+N, A, B, C, D = map(int,input().split())
+#A개를 B원에 C개를 D원
+if B*C < D*A:
+    A, B, C, D = C, D, A, B
+
+#첫 번째 가게가 더 가성비가 좋다
+answer = float("INF")
+
+#A개를 C만큼 사는게 더 이득 => C를 A개보다 많이 사면 손해
+for i in range(C):
+    #C를 i개 만큼 사기
+    temp = math.ceil((N - i * A)/C)
+    tri = False
+    if temp < 0:
+        temp = 0
+        tri = True
+    answer = min(answer, temp * D + i * B)
+    if tri:
         break
-
 print(answer)
