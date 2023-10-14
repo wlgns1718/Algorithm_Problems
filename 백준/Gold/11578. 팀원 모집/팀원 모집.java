6c1776469a2d9ce25390137130c1a7fc8653e19b
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2023-10-14 17:50:17 +0900

    [Gold V] Title: 팀원 모집, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Gold/11578. 팀원 모집/팀원 모집.java
+++ 백준/Gold/11578. 팀원 모집/팀원 모집.java
@@ -3,9 +3,8 @@ import java.io.*;
 
 
 public class Main {
-    static int N, M,cnt, answer = -1,target;
+    static int N, M, answer = 11,target;
     static int[] students;
-    static boolean[] visit;
     public static void main(String[] args) throws Exception {
 //
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,46 +12,31 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        students = new int[M];
-        visit = new boolean[M];
-        target = (int)Math.pow(2,N+1)-1;
-        for(int i = 0; i < M; i++){
+        students = new int[M+1];
+        target = 0;
+        for(int i = 1; i <= N; i++){
+            target |= 1 << i;
+        }
+        for(int i = 1; i <= M; i++){
             st = new StringTokenizer(br.readLine());
             int size = Integer.parseInt(st.nextToken());
             for(int j = 0; j < size; j++){
                 students[i] |= (1 << Integer.parseInt(st.nextToken()));
             }
         }
-        for(int i = 1; i <= M; i++){
-            if(answer != -1) break;
-            cnt = i;
-            sol(0);
-        }
+        sol(1, 0, 0);
+        if(answer == 11) answer = -1;
         System.out.println(answer);
 
     }
-    public static void sol(int idx){
-
-        if(idx == cnt){
-            int temp = 1;
-            for(int i = 0; i < M; i++){
-                if(visit[i]){
-                    temp |= students[i];
-                    if(temp == target) break;
-                }
-            }
-            if(temp == target){
-                answer = idx;
-            }
+    private static void sol(int idx, int cnt, int flag){
+        if (flag == target) {
+            answer = Math.min(answer, cnt);
             return;
         }
 
-
-        for(int i = 0; i < M; i++){
-            if(visit[i])continue;
-            visit[i] = true;
-            sol(idx + 1);
-            visit[i] = false;
+        for (int i = idx; i <= M; i++) {
+            sol(i+1, cnt+1, (flag | students[i]));
         }
     }
 
