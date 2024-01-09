463a11a1551fbd3c37255a796c35c82ca912b85c
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2024-01-09 18:04:53 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 124 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        sol(1);
+        System.out.println(sb.toString());
+
+    }
+    private static void sol(int n){
+        if(n == N){
+            sb.append(" ".repeat(N-n)).append("*".repeat(2*(n-1) + 1)).append("\n");
+            return;
+        }
+        sb.append(" ".repeat(N-n)).append("*".repeat(2*(n-1) + 1)).append("\n");
+        sol(n + 1);
+        sb.append(" ".repeat(N-n)).append("*".repeat(2*(n-1) + 1)).append("\n");
+    }
+}
