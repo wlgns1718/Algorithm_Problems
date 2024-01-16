c0beca389f2131121c90493e87f21f33361b52bd
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2024-01-16 23:46:13 +0900

    [Gold V] Title: 톱니바퀴 (2), Time: 116 ms, Memory: 13184 KB -BaekjoonHub

--- 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.java
+++ 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.java
@@ -8,8 +8,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb1 = new StringBuilder();
         N = Integer.parseInt(br.readLine());
         for(int i = 0; i < N; i++){
             ls.add(new StringBuilder().append(br.readLine()));
@@ -20,14 +18,8 @@ public class Main {
             int idx = Integer.parseInt(input[0]) - 1;
             int order = Integer.parseInt(input[1]);
             spin(idx,order);
-//            for(StringBuilder sb : ls){
-//                System.out.println(sb.toString());
-//            }
-//            System.out.println();
         }
-
         int answer = 0;
-
         for(StringBuilder sb : ls){
             if(sb.charAt(0) == '1'){
                 answer++;
@@ -37,10 +29,6 @@ public class Main {
     }
 
     private static void spin(int idx, int order){
-        //1이면 시계 방향 -1이면 반시계
-        //시계 방향이면 7에 있는걸 0에 넣고 8삭제
-        //반시계 방향이면 0에 있는걸 8에 넣고 0삭제
-
         if (order == 1){
             StringBuilder sb = ls.get(idx);
             char left_char = sb.charAt(6);
@@ -67,7 +55,6 @@ public class Main {
             }
         }
     }
-
     private static void left(int idx, int order){
         StringBuilder sb = ls.get(idx);
         char left_char = sb.charAt(6);
