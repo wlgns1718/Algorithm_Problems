dbfa8a309d3c82d61f94337508617c6080523ab7
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2024-02-28 14:36:29 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 148 ms, Memory: 16096 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -9,35 +9,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int TC = Integer.parseInt(br.readLine());
 
-        dp = new int[10001][3];
+        dp = new int[10001][2];
         init();
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < TC; i++){
             int n = Integer.parseInt(br.readLine());
-            bw.write((dp[n][0] + dp[n][1] + dp[n][2]) + "\n");
+            sb.append((1 + dp[n][0] + dp[n][1]) + "\n");
         }
 
-        bw.flush();
+        System.out.println(sb);
     }
     private static void init(){
 
-        dp[1][0] = 1;
+        dp[1][0] = 0;
         dp[1][1] = 0;
-        dp[1][2] = 0;
 
         dp[2][0] = 1;
-        dp[2][1] = 1;
-        dp[2][2] = 0;
+        dp[2][1] = 0;
 
         dp[3][0] = 1;
         dp[3][1] = 1;
-        dp[3][2] = 1;
 
         for(int i = 4; i <= 10000; i++){
 
-            dp[i][0] = 1;
-            dp[i][1] = dp[i-2][0] + dp[i-2][1];
-            dp[i][2] = dp[i-3][0] + dp[i-3][1] + dp[i-3][2];
+            dp[i][0] = 1 + dp[i-2][0];
+            dp[i][1] = 1 + dp[i-3][0] + dp[i-3][1];
         }
     }
 
