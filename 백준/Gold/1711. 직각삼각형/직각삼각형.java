cbf1ea1550a561ca8d289dd52b6fde53a4767a8b
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2024-03-06 15:08:34 +0900

    [Gold V] Title: 직각삼각형, Time: 2828 ms, Memory: 16524 KB -BaekjoonHub

--- 백준/Gold/1711. 직각삼각형/직각삼각형.java
+++ 백준/Gold/1711. 직각삼각형/직각삼각형.java
@@ -5,6 +5,7 @@ public class Main {
 
     static long[] xrr, yrr;
     static int answer = 0, N, arr[];
+
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,37 +15,32 @@ public class Main {
         xrr = new long[N];
         yrr = new long[N];
         arr = new int[3];
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             String[] input = br.readLine().split(" ");
             xrr[i] = Long.parseLong(input[0]);
             yrr[i] = Long.parseLong(input[1]);
         }
-        sol(0,0);
-        System.out.println(answer);
-    }
-    private static void sol(int idx, int start){
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int p = j + 1; p < N; p++) {
+                    long x1 = xrr[i];
+                    long y1 = yrr[i];
+                    long x2 = xrr[j];
+                    long y2 = yrr[j];
+                    long x3 = xrr[p];
+                    long y3 = yrr[p];
 
-        if(idx == 3){
-            long x1 = xrr[arr[0]];
-            long y1 = yrr[arr[0]];
-            long x2 = xrr[arr[1]];
-            long y2 = yrr[arr[1]];
-            long x3 = xrr[arr[2]];
-            long y3 = yrr[arr[2]];
-            
-            long l1 = (x1-x2) * (x1-x2) + (y1-y2) * (y1-y2);
-            long l2 = (x1-x3) * (x1-x3) + (y1-y3) * (y1-y3);
-            long l3 = (x2-x3) * (x2-x3) + (y2-y3) * (y2-y3);
-            
-            if(l1+l2 == l3 || l2+l3 == l1 || l1+l3 == l2){
-                answer++;
+                    long l1 = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+                    long l2 = (x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3);
+                    long l3 = (x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3);
+
+                    if (l1 + l2 == l3 || l2 + l3 == l1 || l1 + l3 == l2) {
+                        answer++;
+                    }
+                }
             }
-            return;
-        }
 
-        for(int i = start; i < N; i++){
-            arr[idx] = i;
-            sol(idx + 1, i + 1);
         }
+        System.out.println(answer);
     }
 }
