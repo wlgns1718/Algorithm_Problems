a70e52dee8bd95ce1f84a9fcc882989f6c3a9eeb
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2024-03-07 19:46:05 +0900

    [Gold IV] Title: 좋다, Time: 380 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,32 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-
-def is_good(num_list, num):
+def sol(val, arr):
+    global N, nums, count
 
     start = 0
-    end = len(num_list) - 1
-
+    end = len(arr) - 1
     while start < end:
-        a = num_list[start] + num_list[end]
-        if a > num:
-            end -= 1
-        elif a < num:
+        a = arr[start] + arr[end]
+        if a < val:
             start += 1
-        elif a == num:
-            return 1
-    return 0
-
+        elif a > val:
+            end -= 1
+        else:
+            count += 1
+            return
 
 N = int(input())
 nums = list(map(int, input().split()))
 nums.sort()
 count = 0
+for idx, val in enumerate(nums):
+    sol(val, nums[0:idx] + nums[idx+1:])
 
-if len(nums) <= 2:
-    print(0)
-else:
-    for i in range(N):
-        new = nums[:i] + nums[i+1:]
-        count += is_good(new, nums[i])
-    print(count)
+print(count)
