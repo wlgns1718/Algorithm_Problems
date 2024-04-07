296a61d9e8a810545f0e7dbecef3f64159bcf158
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2024-04-07 14:53:08 +0900

    [Silver V] Title: 집합, Time: 1216 ms, Memory: 307172 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -2,37 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static Integer[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    static int N, ls[];
+
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
+        ls = new int[21];
         String[] input;
-        HashSet<Integer> set = new HashSet<>();
 
         for(int i = 0; i < N; i++){
             input = br.readLine().split(" ");
             if(input[0].equals("add")){
-                set.add(Integer.parseInt(input[1]));
+                ls[Integer.parseInt(input[1])] = 1;
             }
             else if(input[0].equals("remove")){
-                set.remove(Integer.parseInt(input[1]));
+                ls[Integer.parseInt(input[1])] = 0;
             }
             else if(input[0].equals("check")){
-                if(set.contains(Integer.parseInt(input[1]))) sb.append(1).append("\n");
+                int idx = Integer.parseInt(input[1]);
+                if(ls[idx] == 1) sb.append(1).append("\n");
                 else sb.append(0).append("\n");
             }
             else if(input[0].equals("toggle")){
-                if(set.contains(Integer.parseInt(input[1]))) set.remove(Integer.parseInt(input[1]));
-                else set.add(Integer.parseInt(input[1]));
+                int idx = Integer.parseInt(input[1]);
+                if(ls[idx] == 1) ls[idx] = 0;
+                else ls[idx] = 1;
             }
             else if(input[0].equals("all")){
-                set.addAll(List.of(arr));
+                Arrays.fill(ls, 1);
             }
             else{
-                set.clear();
+                Arrays.fill(ls,0);
             }
         }
         System.out.println(sb);
