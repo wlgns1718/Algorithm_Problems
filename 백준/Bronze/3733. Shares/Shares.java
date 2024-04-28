cce77a5705c4ad34559d2a40d8f73b775c26a6f5
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2024-04-29 02:11:27 +0900

    [Bronze V] Title: Shares, Time: 120 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        while (true) {
+            String cur = br.readLine();
+            if (cur == null) break;
+            StringTokenizer st = new StringTokenizer(cur);
+            int n = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            sb.append(s / (n+1)).append('\n');
+        }
+        System.out.println(sb);
+
+    }
+
+}
