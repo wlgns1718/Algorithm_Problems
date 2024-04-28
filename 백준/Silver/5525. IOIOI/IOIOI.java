ec18d74d6df76f378ff065d85cc93cc673a0c3d3
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2024-04-29 01:57:08 +0900

    [Silver I] Title: IOIOI, Time: 284 ms, Memory: 23316 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -2,26 +2,71 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    static String IO, word;
+    static int l;
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        l = 2 * N + 1;
+
         StringBuilder sb = new StringBuilder();
         sb.append("I");
         sb.append("OI".repeat(N));
-        String IO = sb.toString();
-        int l = Integer.parseInt(br.readLine());
-        String temp = br.readLine();
-        
-        int answer = 0;
-        int start = 0;
+        IO = sb.toString();
+
+        int len = Integer.parseInt(br.readLine());
+        word = br.readLine();
+
+        int start = find(0);
+        if(start == -1){
+            System.out.println(0);
+            return;
+        }
+        int end = start + l - 1;
+        int answer = 1;
+
         while(true){
-            temp = temp.substring(start);
-            if(!temp.contains(IO)) break;
+            if(end + 2 >= len) break;
+
+            if(word.charAt(end + 1) == 'O' && word.charAt(end + 2) == 'I'){
+                end += 2;
+                answer ++;
+                continue;
+            }
+
+            start = find(end + 1);
+            if(start == -1)break;
+            end = start + l - 1;
+            if(end >= len) break;
             answer++;
-            start = temp.indexOf(IO) + 2;
+
         }
         System.out.println(answer);
+
+    }
+    private static int find(int start){
+
+        while(word.charAt(start) != 'I'){
+            start++;
+            if(start == word.length()) return -1;
+        }
+        while(start + l - 1 < word.length()){
+            int idx = 1;
+            Loop1:
+            for(int i = start + 1; i < start + l; i++){
+
+                if(IO.charAt(idx++) != word.charAt(i)){
+                    start = i;
+                    break Loop1;
+                }
+                if(i == start + l - 1) return start;
+            }
+            while(word.charAt(start) != 'I'){
+                start++;
+                if(start == word.length()) return -1;
+            }
+        }
+        return -1;
     }
 }
