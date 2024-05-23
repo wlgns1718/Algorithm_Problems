b18a9bd977d3627752920a6b2b3fed6af5032adf
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2024-05-23 17:41:18 +0900

    [Gold II] Title: 캔디캔디, Time: 380 ms, Memory: 26668 KB -BaekjoonHub

--- 백준/Gold/2878. 캔디캔디/캔디캔디.java
+++ 백준/Gold/2878. 캔디캔디/캔디캔디.java
@@ -2,23 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static final long MOD = (long)18_446_744_073_709_551_616D;
-    static int M, N,end = 0;
+    static final long MOD = (long) 18_446_744_073_709_551_616f;
+    static int M, N, arr[],end = 0;
     static long answer = 0;
-    static long sum = 0, arr[];
+    static long sum = 0;
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        arr = new long[N];
+        arr = new int[N];
         for(int i = 0; i < N; i++){
-            arr[i] = Long.parseLong(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
             sum += arr[i];
+            end = Math.max(end, arr[i]);
         }
         Arrays.sort(arr);
         sum -= M;
@@ -26,8 +26,7 @@ public class Main {
         for(int i = 0; i < N; i++){
             temp = Math.min(arr[i], sum/(N-i));
             sum -= temp;
-            answer += (temp * temp);
-            answer %= MOD;
+            answer += (temp * temp) % MOD;
         }
         System.out.println(answer);
     }
