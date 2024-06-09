ed1b4f5f4a128012cc2e1192e4a9c9a114bbd2f3
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2024-06-09 14:06:09 +0900

    [Silver II] Title: 전구 길만 걷자, Time: 376 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Silver/17359. 전구 길만 걷자/전구 길만 걷자.java
+++ 백준/Silver/17359. 전구 길만 걷자/전구 길만 걷자.java
@@ -3,43 +3,51 @@ import java.io.*;
 
 public class Main {
     static int N;
-    static String[] bulb;
+    static char[][] bulb;
     static boolean[] visit;
-    static int answer = Integer.MAX_VALUE;
+    static int answer = 0;
+    static int min = Integer.MAX_VALUE;
+    static int[] select;
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
-        bulb = new String[N];
+        bulb = new char[N][2];
         visit = new boolean[N];
+        select = new int[N];
 
         for(int i = 0; i < N; i++){
-            String input = br.readLine();
-            bulb[i] = input;
-        }
-        sol(sb, 0);
-        System.out.println(answer);
-    }
-    private static void sol(StringBuilder sb, int idx){
-        if(idx == N){
             int temp = 0;
-            char pre = sb.charAt(0);
-            for(int i = 1; i < sb.length(); i++){
-                if(pre != sb.charAt(i)){
+            String input = br.readLine();
+            char pre = input.charAt(0);
+            for(int j = 1; j < input.length(); j++){
+                if(pre != input.charAt(j)){
                     temp++;
-                    pre = sb.charAt(i);
+                    pre = input.charAt(j);
                 }
             }
-            answer = Math.min(answer, temp);
+            bulb[i][0] = input.charAt(0);
+            bulb[i][1] = input.charAt(input.length()-1);
+            answer += temp;
+        }
+        sol(0);
+        System.out.println(answer + min);
+    }
+    private static void sol(int idx){
+        if(idx == N){
+            int temp = 0;
+            for(int i = 0; i < N-1; i++){
+                if(bulb[select[i]][1] != bulb[select[i+1]][0]) temp++;
+            }
+            min = Math.min(min, temp);
             return;
         }
         for(int i = 0; i < N; i++){
             if(visit[i]) continue;
-            sb.append(bulb[i]);
             visit[i] = true;
-            sol(sb, idx + 1);
-            sb.delete(sb.length() - bulb[i].length(), sb.length());
+            select[idx] = i;
+            sol(idx + 1);
             visit[i] = false;
         }
     }
