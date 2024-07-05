17db9d2295741d0a0bb70ffb62455555f2eff8e1
신지훈 <113763592+wlgns1718@users.noreply.github.com>
2024-07-05 21:58:30 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 108 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,17 +1,20 @@
 import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 
 public class Main {
-    static int N;
+    static int N, arr[] = new int[21];
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        init();
         N = Integer.parseInt(br.readLine());
-        System.out.println(sol(N));
+        System.out.println(arr[N]);
     }
-    private static long sol(int n){
-        if(n == 1 || n == 0) return n;
-
-        return sol(n-1) + sol(n-2);
+    private static void init(){
+        arr[1] = 1;
+        for(int i = 2; i <= 20; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
     }
 }
